516fe72d735164ef3a94ac6f8949f35c9d4c910b
lokesh-yadav12 <lokesh916635@gmail.com>
2026-01-01 10:31:05 +0530

    Set Template 4 (Tax Invoice) as default template

--- lib/variables.ts
+++ lib/variables.ts
@@ -174,7 +174,7 @@ export const FORM_DEFAULT_VALUES = {
     additionalNotes: "",
     paymentTerms: "",
     totalAmountInWords: "",
-    pdfTemplate: 1,
+    pdfTemplate: 4,
   },
 };
 
@@ -257,6 +257,6 @@ export const FORM_FILL_VALUES = {
     subTotal: "850",
     totalAmount: "850",
     totalAmountInWords: "Eight Hundred Fifty",
-    pdfTemplate: 1,
+    pdfTemplate: 4,
   },
 };

b266fd8fd1b2dace47497dbab93794d374378cfc
lokesh-yadav12 <lokesh916635@gmail.com>
2026-01-04 13:40:36 +0530

    remove next button to summary page

--- app/components/invoice/form/wizard/WizardNavigation.tsx
+++ app/components/invoice/form/wizard/WizardNavigation.tsx
@@ -8,15 +8,18 @@ import { BaseButton } from "@/app/components";
 
 // Contexts
 import { useTranslationContext } from "@/contexts/TranslationContext";
+import { useInvoiceContext } from "@/contexts/InvoiceContext";
 
 // Icons
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight, FileInput } from "lucide-react";
 
 const WizardNavigation = () => {
     const { isFirstStep, isLastStep, handleStep, previousStep, nextStep } =
         useWizard();
 
     const { _t } = useTranslationContext();
+    const { invoicePdfLoading } = useInvoiceContext();
+
     return (
         <div className="flex flex-wrap justify-end gap-3 sm:gap-5">
             {!isFirstStep && (
@@ -24,20 +27,34 @@ const WizardNavigation = () => {
                     tooltipLabel="Go back to the previous step"
                     onClick={previousStep}
                     className="flex-1 sm:flex-none"
+                    type="button"
                 >
                     <ArrowLeft className="h-4 w-4" />
                     {_t("form.wizard.back")}
                 </BaseButton>
             )}
-            <BaseButton
-                tooltipLabel="Go to the next step"
-                disabled={isLastStep}
-                onClick={nextStep}
-                className="flex-1 sm:flex-none"
-            >
-                {_t("form.wizard.next")}
-                <ArrowRight className="h-4 w-4" />
-            </BaseButton>
+            {isLastStep ? (
+                <BaseButton
+                    type="submit"
+                    tooltipLabel="Generate your invoice"
+                    loading={invoicePdfLoading}
+                    loadingText="Generating your invoice"
+                    className="flex-1 sm:flex-none"
+                >
+                    <FileInput />
+                    {_t("actions.generatePdf")}
+                </BaseButton>
+            ) : (
+                <BaseButton
+                    tooltipLabel="Go to the next step"
+                    onClick={nextStep}
+                    className="flex-1 sm:flex-none"
+                    type="button"
+                >
+                    {_t("form.wizard.next")}
+                    <ArrowRight className="h-4 w-4" />
+                </BaseButton>
+            )}
         </div>
     );
 };
